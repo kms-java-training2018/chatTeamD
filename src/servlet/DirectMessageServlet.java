3eecedc024916d6ccc7d903bf7e04013cc5b9867
takatsuka-akihito <takatsuka-akihito@KMS2013-69.kms.local>
2018-05-31 20:02:35 +0900

    directmessageservlet修正しました。

--- src/servlet/DirectMessageServlet.java
+++ src/servlet/DirectMessageServlet.java
@@ -19,7 +19,7 @@ import bean.DirectMessageBean;
 public class DirectMessageServlet extends HttpServlet {
 
 
-	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
 		/*
 		* 画面表示処理
 		*/
