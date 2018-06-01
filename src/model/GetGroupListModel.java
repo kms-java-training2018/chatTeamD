0730766239ab8e4873d70d5b3d53f20d22a0f3ae
masuda-keito <masuda-keito@KMS2013-111.kms.local>
2018-06-01 13:47:09 +0900

    mainpage周りMVC細分化完了

--- src/model/GetGroupListModel.java
+++ src/model/GetGroupListModel.java
@@ -8,7 +8,6 @@ import java.sql.Statement;
 import java.util.ArrayList;
 
 import bean.GroupListBean;
-import bean.UserListBean;
 
 /**
  * 登録されているグループ一覧を取得するモデル。
@@ -48,9 +47,6 @@ public class GetGroupListModel {
 			conn = DriverManager.getConnection(url, user, dbPassword);
 			Statement stmt = conn.createStatement();
 			// SQL作成
-			UserListBean uLBean = new UserListBean();
-			GetUserListModel model = new GetUserListModel();
-			uLBean = model.getUserList(uLBean, sesUserNo);
 			/*
 			 * 自分が参加しているグループ一覧取得
 			 */
@@ -99,14 +95,90 @@ public class GetGroupListModel {
 	}
 
 	/**
-	 * bean内のユーザ一覧と引数のユーザ間で最新のDMを取得し、beanに入れるメソッド。
+	 * bean内のグループ一覧と引数のユーザ間での最新メッセージを取得し、beanに入れるメソッド。
 	 *
-	 * @param bean	……最新のメッセージを取得したいユーザ一覧。入っていない場合sesUserNoを利用しgetUserListメソッドを行う
-	 * @param sesUserNo	……一覧のユーザとメッセージをやり取りしたユーザ。
+	 * @param bean	……最新のメッセージを取得したいグループ一覧。入っていない場合sesUserNoを利用しgetGroupListメソッドを行う
+	 * @param sesUserNo	……グループ一覧とメッセージをやり取りしたユーザ。
 	 * @return	UserListBean型beanに最新メッセージを持たせて返す。
 	 */
 	public GroupListBean getGroupLatestMessage(GroupListBean bean, String sesUserNo) {
+		/*
+		 * メッセージ一覧取得
+		 */
+		// beanにユーザー一覧入っているか確認
+		if (bean.getGroupName() == null) {
+			// 入っていない場合
+			// 取得する
+			GetGroupListModel model = new GetGroupListModel();
+			bean = model.getGroupList(bean, sesUserNo);
+		}
+		StringBuilder sb = new StringBuilder();
+		/*
+		 * DBログイン
+		 */
+		Connection conn = null;
+		String url = "jdbc:oracle:thin:@192.168.51.67:1521:XE";
+		String user = "DEV_TEAM_D";
+		String dbPassword = "D_DEV_TEAM";
+		// JDBCドライバーのロード
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
 
+		// 接続作成
+		try {
+			conn = DriverManager.getConnection(url, user, dbPassword);
+			Statement stmt = conn.createStatement();
+			// SQL作成
+			ArrayList<String> groupMessage = new ArrayList<>();
+			for (int i = 0; i < bean.getGroupNo().size(); i++) {
+				int gN = bean.getGroupNo().get(i);
+				sb.append("SELECT ");
+				sb.append(" message ");
+				sb.append("FROM ");
+				sb.append(" t_message_info ");
+				sb.append("WHERE ");
+				sb.append(" regist_date = ( ");
+				sb.append(" SELECT ");
+				sb.append(" MAX(regist_date) ");
+				sb.append("FROM ");
+				sb.append(" t_message_info ");
+				sb.append("WHERE ");
+				sb.append(" to_send_group_no = '" + gN + "' ");
+				sb.append(" AND delete_flag =  '0' )");
+				// SQL実行
+				ResultSet rs4 = stmt.executeQuery(sb.toString());
+				if (rs4.next()) {
+					// データあり
+					// そのままArrayListに入れる
+					groupMessage.add(rs4.getString("MESSAGE"));
+				} else {
+					// データなし
+					// 会話を始めましょう！
+					groupMessage.add("会話を始めましょう!");
+				}
+				// 初期化
+				sb.delete(0, sb.length());
+			}
+			// beanのフィールドにset
+			bean.setGroupMessage(groupMessage);
+		} catch (SQLException e) {
+			// エラーはすべてここにくる
+			e.printStackTrace();
+			bean.setErrorFlag(1);
+			// SQLの接続は絶対に切断
+		} finally {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+
+		}
+		// sb初期化
+		sb.delete(0, sb.length());
 		return bean;
 	}
 
